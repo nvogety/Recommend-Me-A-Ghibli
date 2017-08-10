51b5efd673ef1f69694da163b9db5ab3a54bf6fc
Neeharika V <neeharules@gmail.com>
2017-08-10 12:21:52 -0700

    print movies based on selection

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,38 @@
 
+var movies = {
+  "happy": ["totoro", "howls", "kiki"],
+  "sad": ["fireflies", "nauticaa", "marnie"],
+  "romantic": ["marnie", "wind", "ponyo"]
+};
+
+
 $(".btn-outline-primary").click(function(){
   $(this).toggleClass("active");
 });
 
-
 $("#select").click(function(){
   var html = "";
-
+  var selected = [];
+  var exists = false;
+  
   $( ".active" ).each(function( index ) {
-    html+='<p>'+$(this).attr('id')+'</p>'
-    console.log(html);
+    var results = movies[$(this).attr('id')];
+    for (i = 0; i < results.length; i++) {
+      for (j = 0; j < selected.length; j++) {
+        if(results[i]==selected[j]){
+          exists = true;
+          break;
+        }
+        exists = false;
+      }
+      if(!exists){
+        selected.push(results[i]);
+        html+='<p>'+results[i]+'</p>'
+      }
+    }
   });
 
   $('#myModal').on('shown.bs.modal', function() {
-
     $('#myModal').find('.modal-body').append(html);
   });
   $("#myModal").modal('show');

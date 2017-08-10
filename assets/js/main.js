5f94306c6bed029ce844f75600b948d423e722c0
Neeharika V <neeharules@gmail.com>
2017-08-10 00:31:49 -0700

    add modal+format buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,26 @@
-// $(".ui.toggle.inverted.button").click(function(){
-// 	alert("button was clicked");
-//   $(this).toggleClass("active");
-// });
 
-// $('.ui.button.toggle').state();
-
-$(".btn").click(function(){
+$(".btn-outline-primary").click(function(){
   $(this).toggleClass("active");
+});
+
+
+$("#select").click(function(){
+  var html = "";
+
+  $( ".active" ).each(function( index ) {
+    html+='<p>'+$(this).attr('id')+'</p>'
+    console.log(html);
+  });
+
+  $('#myModal').on('shown.bs.modal', function() {
+
+    $('#myModal').find('.modal-body').append(html);
+  });
+  $("#myModal").modal('show');
+
+});
+
+$(".close").click(function(){
+  console.log("refreshing now");
+  location.reload();
 });

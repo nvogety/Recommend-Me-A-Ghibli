724411c732b199b2cfc0b5803004b36e2b5427c8
Neeharika V <neeharules@gmail.com>
2017-08-09 18:45:29 -0700

    ditch semantic ui+switch to bootstrap+get buttons toggled

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// $(".ui.toggle.inverted.button").click(function(){
+// 	alert("button was clicked");
+//   $(this).toggleClass("active");
+// });
+
+// $('.ui.button.toggle').state();
+
+$(".btn").click(function(){
+  $(this).toggleClass("active");
+});
